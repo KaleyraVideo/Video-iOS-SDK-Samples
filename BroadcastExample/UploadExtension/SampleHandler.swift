4e8d3c936752cfd8fa7f669dc36bdcb8ab6fc8d5
Marco Brescianini <marco.brescianini@bandyer.com>
2021-06-28 16:16:33 +0200

    Update SampleHandler with calls to BandyerBroadcastExtension framework

--- BroadcastExample/UploadExtension/SampleHandler.swift
+++ BroadcastExample/UploadExtension/SampleHandler.swift
@@ -3,39 +3,30 @@
 //
 
 import ReplayKit
+import BandyerBroadcastExtension
 
 class SampleHandler: RPBroadcastSampleHandler {
 
     override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
-        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional. 
+        #error("Replace APP_GROUP_IDENTIFIER_GOES_HERE placeholder with your app group identifier")
+        BroadcastExtension.instance.start(appGroupIdentifier: "APP_GROUP_IDENTIFIER_GOES_HERE", setupInfo: setupInfo) { [weak self] error in
+            self?.finishBroadcastWithError(error)
+        }
     }
     
     override func broadcastPaused() {
-        // User has requested to pause the broadcast. Samples will stop being delivered.
+        BroadcastExtension.instance.pause()
     }
     
     override func broadcastResumed() {
-        // User has requested to resume the broadcast. Samples delivery will resume.
+        BroadcastExtension.instance.resume()
     }
     
     override func broadcastFinished() {
-        // User has requested to finish the broadcast.
+        BroadcastExtension.instance.finish()
     }
     
     override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
-        switch sampleBufferType {
-        case RPSampleBufferType.video:
-            // Handle video sample buffer
-            break
-        case RPSampleBufferType.audioApp:
-            // Handle audio sample buffer for app audio
-            break
-        case RPSampleBufferType.audioMic:
-            // Handle audio sample buffer for mic audio
-            break
-        @unknown default:
-            // Handle other sample buffer types
-            fatalError("Unknown type of sample buffer")
-        }
+        BroadcastExtension.instance.process(sampleBuffer: sampleBuffer, ofType: sampleBufferType)
     }
 }
