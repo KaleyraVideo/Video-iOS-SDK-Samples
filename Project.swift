be9b9cb6d4550913977bfdb72df13f78d6575637
Marco Brescianini <marco.brescianini@kaleyra.com>
2024-09-03 10:28:57 +0200

    Update project file adding dependencies

--- Project.swift
+++ Project.swift
@@ -2,31 +2,53 @@ import ProjectDescription
 
 let project = Project(
     name: "KaleyraVideo",
+    organizationName: "Kaleyra S.p.a.",
+    options: .options(disableBundleAccessors: true, disableSynthesizedResourceAccessors: true),
     targets: [
         .target(
             name: "KaleyraVideo",
             destinations: .iOS,
             product: .app,
-            bundleId: "io.tuist.KaleyraVideo",
+            bundleId: "com.kaleyra.KaleyraVideo",
             infoPlist: .extendingDefault(
                 with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                    "UIBackgroundModes" : ["audio", "voip", "remote-notification"],
+                    "NSCameraUsageDescription" : "Camera",
+                    "NSMicrophoneUsageDescription" : "Microphone",
+                    "NSPhotoLibraryUsageDescription" : "Photo Library",
+                    "ITSAppUsesNonExemptEncryption" : false
                 ]
             ),
-            sources: ["KaleyraVideo/Sources/**"],
-            resources: ["KaleyraVideo/Resources/**"],
-            dependencies: []
+            sources: ["Source/**"],
+            resources: ["Resources/**"],
+            entitlements: .file(path: .relativeToRoot("Entitlements/SDK Sample.entitlements")),
+            dependencies: [.target(name: "BroadcastExtension"), .external(name: "KaleyraVideoSDK")]
         ),
+        .target(name: "BroadcastExtension",
+                destinations: .iOS,
+                product: .appExtension,
+                bundleId: "com.kaleyra.KaleyraVideo.BroadcastExtension",
+                infoPlist: .file(path: "BroadcastExtension/Info.plist"),
+                sources: ["BroadcastExtension/**"],
+                entitlements: "BroadcastExtension/BroadcastExtension.entitlements"),
         .target(
-            name: "KaleyraVideoTests",
+            name: "KaleyraVideoUnitTests",
             destinations: .iOS,
             product: .unitTests,
-            bundleId: "io.tuist.KaleyraVideoTests",
+            bundleId: "com.kaleyra.KaleyraVideoUnitTests",
             infoPlist: .default,
-            sources: ["KaleyraVideo/Tests/**"],
+            sources: ["Tests/UnitTests/**", "Testing/**"],
+            resources: [],
+            dependencies: [.target(name: "KaleyraVideo")]
+        ),
+        .target(
+            name: "KaleyraVideoIntegrationTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "com.kaleyra.KaleyraVideoIntegrationTests",
+            infoPlist: .default,
+            sources: ["Tests/IntegrationTests/**", "Testing/**"],
             resources: [],
             dependencies: [.target(name: "KaleyraVideo")]
         ),
