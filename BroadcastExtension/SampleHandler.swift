69e33d2610bd2a0428929ae3351f5a6fc65aba4f
Marco Brescianini <marco.brescianini@kaleyra.com>
2024-11-12 10:01:24 +0100

    Update SampleHandler adding log level

--- BroadcastExtension/SampleHandler.swift
+++ BroadcastExtension/SampleHandler.swift
@@ -8,37 +8,30 @@ import KaleyraVideoBroadcastExtension
 import BandyerBroadcastExtension
 #endif
 
-class SampleHandler: RPBroadcastSampleHandler {
+final class SampleHandler: RPBroadcastSampleHandler {
 
     override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
-        guard #available(iOSApplicationExtension 15.0, *) else { return }
-
+#if canImport(KaleyraVideoBroadcastExtension)
+        BroadcastExtension.logLevel = .all
+#endif
         BroadcastExtension.instance.start(appGroupIdentifier: "group.com.bandyer.BandyerSDKSample", setupInfo: nil) { [unowned self] error in
             self.finishBroadcastWithError(error)
         }
     }
 
     override func broadcastPaused() {
-        guard #available(iOSApplicationExtension 15.0, *) else { return }
-
         BroadcastExtension.instance.pause()
     }
 
     override func broadcastResumed() {
-        guard #available(iOSApplicationExtension 15.0, *) else { return }
-
         BroadcastExtension.instance.resume()
     }
 
     override func broadcastFinished() {
-        guard #available(iOSApplicationExtension 15.0, *) else { return }
-
         BroadcastExtension.instance.finish()
     }
 
     override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
-        guard #available(iOSApplicationExtension 15.0, *) else { return }
-
         BroadcastExtension.instance.process(sampleBuffer: sampleBuffer, ofType: sampleBufferType)
     }
 }
